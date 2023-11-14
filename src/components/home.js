da025ed115541aa7a1effacf2587faf12c95cde9
manish1034 <76244041+manish1034@users.noreply.github.com>
2023-11-15 02:39:03 +0530

    aa

--- src/components/home.js
+++ src/components/home.js
@@ -1,49 +1,79 @@
-import React,{useEffect, useState} from 'react';
-import {VscSettings} from 'react-icons/vsc';
-import {IoIosArrowDown} from 'react-icons/io';
-import FilterView from './filterView';
-
+import React, { useEffect, useState } from "react";
+import { VscSettings } from "react-icons/vsc";
+import { IoIosArrowDown } from "react-icons/io";
+import FilterView from "./filterView";
 
 const Home = () => {
   const [menu, setMenu] = useState(false);
+  
   const [filter, setFilter] = useState({
-    grouping: '',
-    ordering: ''
-  })
+    grouping: "",
+    ordering: "",
+  });
   const handleChange = (e) => {
-    const {name, value} = e.target;
-    setFilter({...filter, [name] : value});
-  }
+    const { name, value } = e.target;
+    setFilter({ ...filter, [name]: value });
+  };
+
+
   useEffect(() => {
-    setFilter({...filter, grouping : "userId"})
-  },[])
+    const items = JSON.parse(localStorage.getItem("filter"));
+    if (items) {
+      setFilter(items);
+    }
+  }, []);
+  
   return (
     <>
-        <div className='home-heading'>
-            <div className='menu' onClick={()=>setMenu(!menu)}><VscSettings className='icon'/>Display<IoIosArrowDown/></div>
-            {menu && (
-            <div className='open-menu'>
-                <div className='main-select'><span className='text'>Grouping</span> 
-                    <select className='select' value={filter.grouping} name='grouping' onClick={() => setFilter({...filter, ordering:""})} onChange={handleChange}>
-                        <option value='status'>Status</option>
-                        <option value='userId'>User</option>
-                        <option value='priority'>Priority</option>
-                    </select>
-                </div>
-                <div className='main-select'><span className='text'>Ordering</span> 
-                    <select className='select' name='ordering' onChange={handleChange} value={filter.ordering}>
-                        <option value=''>no order</option>
-                        <option value='title'>Title</option>
-                        <option value='priority'>Priority</option>
-                    </select>
-                </div>
-            </div>)}
+      <div className="home-heading">
+        <div className="menu" onClick={() => setMenu(!menu)}>
+          <VscSettings className="icon" />
+          Display
+          <IoIosArrowDown />
         </div>
-        <div className='body'>
-            <FilterView group={filter.grouping} order={filter.ordering} />
-        </div>
+        {menu && (
+          <div className="open-menu">
+            <div className="main-select">
+              <span className="text">Grouping</span>
+              <select
+                className="select"
+                value={filter.grouping}
+                name="grouping"
+                onChange={handleChange}
+                onClick={() => {
+                  setFilter({ ...filter, ordering: "" });
+                  localStorage.setItem("filter", JSON.stringify(filter));
+                }}
+              >
+                <option value="status">Status</option>
+                <option value="userId">User</option>
+                <option value="priority">Priority</option>
+              </select>
+            </div>
+            <div className="main-select">
+              <span className="text">Ordering</span>
+              <select
+                className="select"
+                value={filter.ordering}
+                name="ordering"
+                onChange={(e) => {
+                  handleChange(e);
+                  localStorage.setItem("filter", JSON.stringify(filter));
+                }}
+              >
+                <option value="">no order</option>
+                <option value="title">Title</option>
+                <option value="priority">Priority</option>
+              </select>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="body">
+        <FilterView group={filter.grouping} order={filter.ordering} />
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
